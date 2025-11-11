71ad59e342b6577862844e9d4ee4e0da2f28ec4f
lheming2 <lheming2@uni-muenster.de>
2025-11-11 14:49:54 +0100

    added basic website functionality

---
+++ frontend/landingpage.js
@@ -0,0 +1,2 @@
+document.getElementById("content").innerHTML = `<h1>Hi!</h1>
+  <p>Welcome.</p>`;
---
+++ frontend/routingpage.js
@@ -0,0 +1,4 @@
+document.getElementById("content").innerHTML = `
+  <h1>Routing</h1>
+  <p>Look here for a route to your destination.</p>
+`;
