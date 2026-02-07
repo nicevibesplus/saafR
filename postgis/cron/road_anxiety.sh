d0be95d20b5d2fcad8efa132848a62243bbcfa48
JanB <133894095+job2002@users.noreply.github.com>
2026-02-07 18:32:40 +0100

    cleanup; cron now pulls from env variables

--- postgis/cron/road_anxiety.sh
+++ postgis/cron/road_anxiety.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-# Container-ENV explizit übernehmen
-export POSTGRES_USER=${POSTGRES_USER:-admin}
-export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-admin}
-export POSTGRES_DB=${POSTGRES_DB:-saafr}
-export POSTGRES_SERVER=${POSTGRES_SERVER:-postgis}
-export POSTGRES_PORT=${POSTGRES_PORT:-5432}
+source /container.env
 
 echo "$(date) - Start Update"
 
-psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:$POSTGRES_PORT/$POSTGRES_DB" \
-     -f /docker-entrypoint-initdb.d/07_calc_road_anxiety_match.sql
+export PGHOST="$POSTGRES_SERVER"
+export PGPORT="$POSTGRES_PORT"
+export PGUSER="$POSTGRES_USER"
+export PGPASSWORD="$POSTGRES_PASSWORD"
+export PGDATABASE="$POSTGRES_DB"
 
+psql -f /docker-entrypoint-initdb.d/07_calc_road_anxiety_match.sql  
+      
 echo "$(date) - Updating table finished."
 
 echo "$(date)" > /shared/road_anxiety.done
